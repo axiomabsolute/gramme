7a305d7ff847e66db8cc854e7df0cc65c7365747
axiom <axiomabsolute@gmail.com>
2021-02-10 18:21:12 -0800

    Add default lexicon

--- comlang/lexicon.go
+++ comlang/lexicon.go
@@ -4,3 +4,22 @@ import "github.com/axiomabsolute/gramme/comlang/pos"
 
 // Lexicon - Map from PoS -> word name -> implementation
 type Lexicon map[pos.Tag]map[string]interface{}
+
+func DefaultLexicon() Lexicon {
+	return map[pos.Tag]map[string]interface{}{
+		pos.Verb: map[string]interface{}{
+			"change": "",
+			"delete": "",
+			"exit":   "",
+		},
+		pos.Preposition: map[string]interface{}{
+			"inside": "",
+			"around": "",
+		},
+		pos.TextObject: map[string]interface{}{
+			"buffer": "",
+			"line":   "",
+			"word":   "",
+		},
+	}
+}
