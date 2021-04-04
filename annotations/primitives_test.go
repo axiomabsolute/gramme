cb9f696e54c64541587851b4c5faac43aca24102
Devon <axiomabsolute@gmail.com>
2021-04-04 10:17:18 -0700

    Test coverage

---
+++ annotations/primitives_test.go
@@ -0,0 +1,26 @@
+package annotations
+
+import (
+	"testing"
+
+	"github.com/axiomabsolute/gramme/primitives"
+)
+
+func TestGetAnnotatedText(t *testing.T) {
+	text := "foo bar baz"
+	annotation := Annotation{Tag: WORD, Region: primitives.Region{Left: primitives.Span{A: 3, B: 4}, Right: primitives.Span{A: 7, B: 8}}}
+	result := GetAnnotatedText(text, annotation)
+	expectedInner := "bar"
+	if len(result) != 3 {
+		t.Errorf("Expected 3 results; prefix inner suffix")
+	}
+	if result[0] != " " {
+		t.Errorf("Expected prefix ` `")
+	}
+	if result[1] != expectedInner {
+		t.Errorf("Expected inner `%s`", expectedInner)
+	}
+	if result[2] != " " {
+		t.Errorf("Expected suffix ` `")
+	}
+}
